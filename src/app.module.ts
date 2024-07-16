23b1c91ec5198d715980eb37a5b01822535e0ef7
best <2907647445@qq.com>
2024-07-16 20:36:24 +0800

    创建实体

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,23 @@ import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [UsersModule, PostsModule, AuthModule,
-    TypeOrmModule.forRoot({
-      type:'postgres',
-      entities:[],
-      synchronize:true,
-      port:5432,
-      username:'postgres',
-      password:'123456',
-      host:'localhost',
-      database:'nestjs-blog'
+    TypeOrmModule.forRootAsync({
+      imports:[],
+      inject:[],
+      useFactory:()=>({
+        type:'postgres',
+        entities:[User],
+        synchronize:true,
+        port:5432,
+        username:'postgres',
+        password:'123456',
+        host:'localhost',
+        database:'nestjs-blog'
+      })
     })
   ],
   controllers: [AppController],
--- src/users/dtos/create-users.dto.ts
+++ src/users/dtos/create-users.dto.ts
@@ -6,22 +6,24 @@ export class CreateUsersDto{
         message:'firstName必填'
     })
     @MinLength(3)
-    @MaxLength(90)
+    @MaxLength(96)
     firstName:string;
 
     @IsString()
     @IsOptional()
     @MinLength(3)
-    @MaxLength(90)
+    @MaxLength(96)
     lastName?:string;
 
     @IsEmail()
     @IsNotEmpty()
+    @MaxLength(96)
     email:string;
 
     @IsString()
     @IsNotEmpty()
     @MinLength(8)
+    @MaxLength(96)
     @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[`~!@#$%^&*()_+<>?:"{},.\/\\;'[\]])[A-Za-z\d`~!@#$%^&*()_+<>?:"{},.\/\\;'[\]]{8,}$/,{
         message:'至少8位且必有数字+特殊字符+字母'
     })
